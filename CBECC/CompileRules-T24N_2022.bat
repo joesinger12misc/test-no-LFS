a70efd280a38b7879977cb86653810f956a353df
Scott Criswell <scott.a.criswell@gmail.com>
2025-02-25 02:42:43 +0000

    SVN (com) commit #8757 by scriswellwsf       revisions to ruleset source directories and corresponding updates to rule compilation and screen file copying .bat files

--- CBECC/CompileRules-T24N_2022.bat
+++ CBECC/CompileRules-T24N_2022.bat
@@ -1,19 +1,20 @@
 if not exist "Data\Rulesets" mkdir "Data\Rulesets"
 if not exist "Data\Rulesets\T24N_2022" mkdir "Data\Rulesets\T24N_2022"
-if exist BEMCompiler22c.exe BEMCompiler22c.exe --sharedPath1="../RulesetDev/Rulesets/shared/" --bemBaseTxt="../RulesetDev/Rulesets/BEMBase.txt" --bemEnumsTxt="../RulesetDev/Rulesets/T24N/T24N_2022 BEMEnums.txt" --bemBaseBin="Data/Rulesets/T24N_2022/T24N_2022 BEMBase.bin" --rulesTxt="../RulesetDev/Rulesets/T24N/Rules/T24N_2022.txt" --rulesBin="Data/Rulesets/T24N_2022.bin" --rulesLog="_T24N-2022 Rules Log.out" --compileDM --compileRules
+if exist BEMCompiler22c.exe BEMCompiler22c.exe --sharedPath1="../RulesetSrc/shared/" --bemBaseTxt="../RulesetSrc/BEMBase.txt" --bemEnumsTxt="../RulesetSrc/T24NRMF/T24N_2022 BEMEnums.txt" --bemBaseBin="Data/Rulesets/T24N_2022/T24N_2022 BEMBase.bin" --rulesTxt="../RulesetSrc/T24NRMF/T24N_2022.txt" --rulesBin="Data/Rulesets/T24N_2022.bin" --rulesLog="_T24N-2022 Rules Log.out" --compileDM --compileRules
 echo OFF
 echo BEMCompiler22c.exe returned (%ERRORLEVEL%) for T24N_2022
 if %ERRORLEVEL%==0 goto :copyfiles
 goto :error
 :copyfiles
-copy "..\RulesetDev\Rulesets\T24N\T24N_2022 Screens.txt"  "..\CBECC\Data\Rulesets\T24N_2022\T24N_2022 Screens.txt"
-copy "..\RulesetDev\Rulesets\T24N\T24N ToolTips.txt" "..\CBECC\Data\Rulesets\T24N_2022\T24N_2022 ToolTips.txt"
-copy "..\RulesetDev\Rulesets\T24N\*.jpg" "..\CBECC\Data\Rulesets\T24N_2022\*.*"
-copy "..\RulesetDev\Rulesets\T24N\RTF\*.*" "..\CBECC\Data\Rulesets\T24N_2022\RTF\*.*"
-copy "..\RulesetDev\Rulesets\shared\Screens*.txt" "..\CBECC\Data\Rulesets\T24N_2022\*.*"
-copy "..\RulesetDev\Rulesets\shared\*.jpg" "..\CBECC\Data\Rulesets\T24N_2022\*.*"
-rem copy "..\RulesetDev\Rulesets\CEC 2013 Nonres\CEC 2013 NonRes Defaults.dbd" "..\CBECC-Com\Data\Rulesets\T24N_2022\T24N_2022 Defaults.dbd"
-rem copy "..\RulesetDev\Rulesets\T24N\DHWDU2.txt" "..\CBECC\CSE\*.*"
+copy "..\RulesetSrc\T24NRMF\T24N_2022 Screens.txt"  "..\CBECC\Data\Rulesets\T24N_2022\T24N_2022 Screens.txt"
+copy "..\RulesetSrc\T24NRMF\T24N ToolTips.txt" "..\CBECC\Data\Rulesets\T24N_2022\T24N_2022 ToolTips.txt"
+copy "..\RulesetSrc\T24NRMF\*.jpg" "..\CBECC\Data\Rulesets\T24N_2022\*.*"
+copy "..\RulesetSrc\T24NRMF\RTF\*.*" "..\CBECC\Data\Rulesets\T24N_2022\RTF\*.*"
+copy "..\RulesetSrc\shared\RTF\*.*" "..\CBECC\Data\Rulesets\T24N_2022\RTF\*.*"
+copy "..\RulesetSrc\shared\Screens*.txt" "..\CBECC\Data\Rulesets\T24N_2022\*.*"
+copy "..\RulesetSrc\shared\*.jpg" "..\CBECC\Data\Rulesets\T24N_2022\*.*"
+rem copy "..\RulesetSrc\CEC 2013 Nonres\CEC 2013 NonRes Defaults.dbd" "..\CBECC-Com\Data\Rulesets\T24N_2022\T24N_2022 Defaults.dbd"
+rem copy "..\RulesetSrc\T24NRMF\DHWDU2.txt" "..\CBECC\CSE\*.*"
 goto :done
 :error
 echo -
--- CBECC/CompileRules-T24_2025.bat
+++ CBECC/CompileRules-T24_2025.bat
@@ -1,19 +1,20 @@
 if not exist "Data\Rulesets" mkdir "Data\Rulesets"
 if not exist "Data\Rulesets\T24_2025" mkdir "Data\Rulesets\T24_2025"
-if exist BEMCompiler25c.exe BEMCompiler25c.exe --sharedPath1="../RulesetDev/Rulesets/shared/" --bemBaseTxt="../RulesetDev/Rulesets/BEMBase.txt" --bemEnumsTxt="../RulesetDev/Rulesets/T24N/T24N_2025 BEMEnums.txt" --bemBaseBin="Data/Rulesets/T24_2025/T24_2025 BEMBase.bin" --rulesTxt="../RulesetDev/Rulesets/T24N/Rules/T24N_2025.txt" --rulesBin="Data/Rulesets/T24_2025.bin" --rulesLog="_T24-2025 Rules Log.out" --compileDM --compileRules
+if exist BEMCompiler25c.exe BEMCompiler25c.exe --sharedPath1="../RulesetSrc/shared/" --bemBaseTxt="../RulesetSrc/BEMBase.txt" --bemEnumsTxt="../RulesetSrc/T24NRMF/T24N_2025 BEMEnums.txt" --bemBaseBin="Data/Rulesets/T24_2025/T24_2025 BEMBase.bin" --rulesTxt="../RulesetSrc/T24NRMF/T24N_2025.txt" --rulesBin="Data/Rulesets/T24_2025.bin" --rulesLog="_T24-2025 Rules Log.out" --compileDM --compileRules
 echo OFF
 echo BEMCompiler25c.exe returned (%ERRORLEVEL%) for T24N_2025
 if %ERRORLEVEL%==0 goto :copyfiles
 goto :error
 :copyfiles
-copy "..\RulesetDev\Rulesets\T24N\T24N_2025 Screens.txt"  "..\CBECC\Data\Rulesets\T24_2025\T24_2025 Screens.txt"
-copy "..\RulesetDev\Rulesets\T24N\T24N ToolTips.txt" "..\CBECC\Data\Rulesets\T24_2025\T24_2025 ToolTips.txt"
-copy "..\RulesetDev\Rulesets\T24N\*.jpg" "..\CBECC\Data\Rulesets\T24_2025\*.*"
-copy "..\RulesetDev\Rulesets\T24N\RTF\*.*" "..\CBECC\Data\Rulesets\T24_2025\RTF\*.*"
-copy "..\RulesetDev\Rulesets\shared\Screens*.txt" "..\CBECC\Data\Rulesets\T24_2025\*.*"
-copy "..\RulesetDev\Rulesets\shared\*.jpg" "..\CBECC\Data\Rulesets\T24_2025\*.*"
-rem copy "..\RulesetDev\Rulesets\CEC 2013 Nonres\CEC 2013 NonRes Defaults.dbd" "..\CBECC-Com\Data\Rulesets\T24_2025\T24_2025 Defaults.dbd"
-rem copy "..\RulesetDev\Rulesets\T24N\DHWDU2.txt" "..\CBECC\CSE\*.*"
+copy "..\RulesetSrc\T24NRMF\T24N_2025 Screens.txt"  "..\CBECC\Data\Rulesets\T24_2025\T24_2025 Screens.txt"
+copy "..\RulesetSrc\T24NRMF\T24N ToolTips.txt" "..\CBECC\Data\Rulesets\T24_2025\T24_2025 ToolTips.txt"
+copy "..\RulesetSrc\T24NRMF\*.jpg" "..\CBECC\Data\Rulesets\T24_2025\*.*"
+copy "..\RulesetSrc\T24NRMF\RTF\*.*" "..\CBECC\Data\Rulesets\T24_2025\RTF\*.*"
+copy "..\RulesetSrc\shared\RTF\*.*" "..\CBECC\Data\Rulesets\T24_2025\RTF\*.*"
+copy "..\RulesetSrc\shared\Screens*.txt" "..\CBECC\Data\Rulesets\T24_2025\*.*"
+copy "..\RulesetSrc\shared\*.jpg" "..\CBECC\Data\Rulesets\T24_2025\*.*"
+rem copy "..\RulesetSrc\CEC 2013 Nonres\CEC 2013 NonRes Defaults.dbd" "..\CBECC-Com\Data\Rulesets\T24_2025\T24_2025 Defaults.dbd"
+rem copy "..\RulesetSrc\T24NRMF\DHWDU2.txt" "..\CBECC\CSE\*.*"
 goto :done
 :error
 echo -
