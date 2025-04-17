f842b969ffb0d66b516500f74a97a19166ab37bd
Scott Criswell <scott.a.criswell@gmail.com>
2025-04-17 04:45:57 +0000

    SVN (com) commit #8789 by scriswellwsf       CBECC 2022/25 executables, CSE v0.925.0, ruleset source and UI updates enabling residential split heat pump and air cond simulation in CSE via new Performance Map model (based on NEEP database)

--- CBECC/CopyScreens_T24N_2025.bat
+++ CBECC/CopyScreens_T24N_2025.bat
@@ -3,5 +3,5 @@ copy "..\RulesetSrc\T24NRMF\T24N ToolTips.txt" "..\CBECC\Data\Rulesets\T24_2025\
 copy "..\RulesetSrc\T24NRMF\*.jpg" "..\CBECC\Data\Rulesets\T24_2025\*.*"
 copy "..\RulesetSrc\T24NRMF\RTF\*.*" "..\CBECC\Data\Rulesets\T24_2025\RTF\*.*"
 copy "..\RulesetSrc\shared\RTF\*.*" "..\CBECC\Data\Rulesets\T24_2025\RTF\*.*"
-copy "..\RulesetSrc\shared\Screens_Res.txt" "..\CBECC\Data\Rulesets\T24_2025\*.*"
+copy "..\RulesetSrc\shared\Screens_Res_2025.txt" "..\CBECC\Data\Rulesets\T24_2025\*.*"
 copy "..\RulesetSrc\shared\*.jpg" "..\CBECC\Data\Rulesets\T24_2025\*.*"
