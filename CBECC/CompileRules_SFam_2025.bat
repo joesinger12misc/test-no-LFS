90fee83a991ffab5576dd70b20c6734a0fbfe786
SACriswell <scott.a.criswell@gmail.com>
2025-10-12 13:01:40 -0700

    CUAC rate updates 2025.09

--- CBECC/CompileRules_SFam_2025.bat
+++ CBECC/CompileRules_SFam_2025.bat
@@ -1,7 +1,7 @@
 echo off
   echo --------------------------------------------
   echo Compiling 2025 SFam ruleset...
-BEMCompiler25.exe --bemBaseTxt="../RulesetSrc/BEMBase-SFam.txt" --bemEnumsTxt="../RulesetSrc/T24SFam/CAR25 BEMEnums.txt" --bemBaseBin="Data/Rulesets/CA Res 2025/CAR25 BEMBase.bin" --rulesTxt="../RulesetSrc/T24SFam/Rules-2025.txt" --rulesBin="Data/Rulesets/CA Res 2025.bin" --rulesLog="_Rules-SFam-2025 Log.out" --compileDM --compileRules
+BEMCompiler25.exe --sharedPath1="../RulesetSrc/shared/" --bemBaseTxt="../RulesetSrc/BEMBase-SFam.txt" --bemEnumsTxt="../RulesetSrc/T24SFam/CAR25 BEMEnums.txt" --bemBaseBin="Data/Rulesets/CA Res 2025/CAR25 BEMBase.bin" --rulesTxt="../RulesetSrc/T24SFam/Rules-2025.txt" --rulesBin="Data/Rulesets/CA Res 2025.bin" --rulesLog="_Rules-SFam-2025 Log.out" --compileDM --compileRules
 echo BEMCompiler25.exe returned (%ERRORLEVEL%) for CA Res 2025
 if %ERRORLEVEL%==0 goto :copyfiles2
 goto :error2
